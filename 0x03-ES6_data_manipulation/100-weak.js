60024c693c8fb78fb2defbea883881bb479cd496
Mohamed Ayman <mohayman2007@gmail.com>
2024-04-17 17:49:09 +0000

    0x03. ES6 data manipulation - Task 11 - minor fix

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,11 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint);
+  let count = weakMap.get(endpoint);
 
-  if (!count) weakMap.set(endpoint, 1);
-  else if (count < 5) weakMap.set(endpoint, count + 1);
-  else throw new Error('Endpoint load is high');
+  if (!count) count = 0;
+  count += 1;
+  weakMap.set(endpoint, count);
+
+  if (count >= 5) throw new Error('Endpoint load is high');
 }
