7372258a6c796f35978926743c6c2f917c369f89
Mohamed Ayman <mohayman2007@gmail.com>
2024-06-05 02:19:22 +0000

    0x05. NodeJS Basics - Task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,50 @@
-// const process = require('process');
+const fs = require('fs');
 
-function displayMessage(string) {
-  console.log(string);
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    const lines = data.trim().split('\n');
+    fieldsNames = lines[0].split(',');
+
+    const students = [];
+
+    lines.slice(1).forEach((element) => {
+      const obj = {};
+
+      const values = element.split(',')
+      for (let i = 0 ; i < fieldsNames.length ; i++) {
+        obj[fieldsNames[i]] = values[i];
+      }
+
+      students.push(obj);
+    });
+
+    console.log(`Number of students: ${students.length}`);
+    const fieldsStats = {};
+    
+    for (student of students){
+      stat = fieldsStats[student.field];
+      if (stat === undefined) {
+        fieldsStats[student.field] = {
+          count: 0,
+          list: []
+        };
+      }
+
+      fieldsStats[student.field].count++; 
+      fieldsStats[student.field].list.push(student.firstname);
+    }
+
+    Object.keys(fieldsStats).forEach((field) => {
+      const count = fieldsStats[field].count;
+      const list = fieldsStats[field].list;
+
+      console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+    });
+  } catch (err) {
+    console.error('Cannot load the database');
+  }
 }
 
-module.exports = displayMessage;
+module.exports = countStudents;
