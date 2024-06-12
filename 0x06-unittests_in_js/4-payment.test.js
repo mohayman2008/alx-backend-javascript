e3645dbed17169026a58b84737f0338ef4040a6f
Mohamed Ayman <mohayman2007@gmail.com>
2024-06-12 06:55:42 +0000

    0x06. Unittests in JS - Task 4 - Handling faulty checkers #5

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,17 +3,18 @@ const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
-describe('sendPaymentRequestToApi', function () {
-  it('calls console.log and Utils.calculateNumber with the correct args', function () {
-    const conSpy = sinon.spy(console, 'log');
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
 
+    dummy.returns(10);
     sendPaymentRequestToApi(100, 20);
-
-    expect(conSpy.calledOnceWith('The total is: 10')).to.be.true;
-    expect(stub.calledOnceWith('SUM', 100, 20)).to.be.true;
-
-    stub.restore();
-    conSpy.restore();
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
   });
 });
