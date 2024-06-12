027e81285c6aa0d9c91957c45448ce3669ab29b6
Mohamed Ayman <mohayman2007@gmail.com>
2024-06-12 07:17:25 +0000

    0x06. Unittests in JS - Task 4 - Handling faulty checkers #11

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,8 +1,7 @@
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${total}`);
+  console.log(`The total is: ${Utils.calculateNumber('SUM', totalAmount, totalShipping)}`);
 }
 
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,20 +1,27 @@
+const { expect } = require('chai');
 const sinon = require('sinon');
 const Utils = require('./utils');
-const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./4-payment');
 
-describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
-    const bigBrother = sinon.spy(console);
-    const dummy = sinon.stub(Utils, 'calculateNumber');
+/* eslint-disable no-undef */
+describe('sendPaymentRequestToApi', function () {
+  before(function () {
+    sinon.spy(console, 'log');
+    sinon.stub(Utils, 'calculateNumber').returns(10);
+  });
 
-    dummy.returns(10);
+  after(function () {
+    Utils.calculateNumber.restore();
+    console.log.restore();
+  });
+
+  it('Correct output', function () {
     sendPaymentRequestToApi(100, 20);
-    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
-    expect(dummy.callCount).to.be.equal(1);
-    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
-    expect(bigBrother.log.callCount).to.be.equal(1);
-    dummy.restore();
-    bigBrother.log.restore();
+
+    expect(console.log.calledOnceWith('The total is: 10')).to.be.true;
+  });
+
+  it('Utils.calculateNumber is called with the right args', function () {
+    expect(Utils.calculateNumber.calledOnceWith('SUM', 100, 20)).to.be.true;
   });
 });
