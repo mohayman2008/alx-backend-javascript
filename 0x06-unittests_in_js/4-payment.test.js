d3cab02edc079cb6d1d5b8830f009a04f7733e56
Mohamed Ayman <mohayman2007@gmail.com>
2024-06-12 06:35:06 +0000

    0x06. Unittests in JS - Task 4 - Fixing #1

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,24 +3,25 @@ const sinon = require('sinon');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
-describe("sendPaymentRequestToApi", function () {
+/* eslint-disable no-undef */
+describe('sendPaymentRequestToApi', function () {
   before(function () {
     sinon.spy(console, 'log');
     sinon.stub(Utils, 'calculateNumber').returns(10);
   });
-  
+
   after(function () {
     Utils.calculateNumber.restore();
     console.log.restore();
   });
-  
-  it("Correct output", function () {
+
+  it('Correct output', function () {
     sendPaymentRequestToApi(100, 20);
 
-    expect(console.log.calledWith('The total is: 10')).to.be.true;
+    expect(console.log.calledOnceWith('The total is: 10')).to.be.true;
   });
 
   it('Utils.calculateNumber is called with the right args', function () {
     expect(Utils.calculateNumber.calledOnceWith('SUM', 100, 20)).to.be.true;
-  })
+  });
 });
